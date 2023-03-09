cd055c120b9ab629d2346bf865da38fd8350865b
Chopperkasa <limaszandresfelipe@gmail.com>
2023-03-09 13:35:38 -0500

    Se añaden las imagenes necesarias

--- src/interfaz/EstadisticaUI.java
+++ src/interfaz/EstadisticaUI.java
@@ -37,7 +37,7 @@ public class EstadisticaUI extends JFrame implements ActionListener {
 	}
 	
 	private void iniciarComponentes() {
-		setBounds(100, 100, 254, 197);
+		setBounds(100, 100, 367, 197);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -46,43 +46,43 @@ public class EstadisticaUI extends JFrame implements ActionListener {
 		
 		btnVolver = new JButton("Volver");
 		btnVolver.addActionListener(this);
-		btnVolver.setBounds(84, 124, 89, 23);
+		btnVolver.setBounds(132, 124, 89, 23);
 		contentPane.add(btnVolver);
 		
 		JLabel lblTitulo = new JLabel("Estadisticas");
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitulo.setFont(new Font("Georgia", Font.BOLD, 18));
-		lblTitulo.setBounds(10, 11, 235, 23);
+		lblTitulo.setBounds(58, 11, 235, 23);
 		contentPane.add(lblTitulo);
 		
 		JLabel lblDistancia = new JLabel("Distancia total recorrida:");
 		lblDistancia.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDistancia.setBounds(30, 45, 135, 14);
+		lblDistancia.setBounds(10, 45, 203, 14);
 		contentPane.add(lblDistancia);
 		
 		JLabel lblVelocidadP = new JLabel("Velocidad promedio:");
 		lblVelocidadP.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblVelocidadP.setBounds(30, 70, 135, 14);
+		lblVelocidadP.setBounds(10, 70, 203, 14);
 		contentPane.add(lblVelocidadP);
 		
 		JLabel lblTiempoKm = new JLabel("Tiempo por kilometro promedio:");
 		lblTiempoKm.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblTiempoKm.setBounds(10, 95, 155, 14);
+		lblTiempoKm.setBounds(10, 95, 203, 14);
 		contentPane.add(lblTiempoKm);
 		
 		lblDistanciaR = new JLabel("New label");
 		lblDistanciaR.setHorizontalAlignment(SwingConstants.LEFT);
-		lblDistanciaR.setBounds(175, 45, 70, 14);
+		lblDistanciaR.setBounds(223, 45, 118, 14);
 		contentPane.add(lblDistanciaR);
 		
 		lblVelocidadPR = new JLabel("New label");
 		lblVelocidadPR.setHorizontalAlignment(SwingConstants.LEFT);
-		lblVelocidadPR.setBounds(175, 70, 70, 14);
+		lblVelocidadPR.setBounds(223, 70, 118, 14);
 		contentPane.add(lblVelocidadPR);
 		
 		lblTiempoKmR = new JLabel("New label");
 		lblTiempoKmR.setHorizontalAlignment(SwingConstants.LEFT);
-		lblTiempoKmR.setBounds(175, 95, 70, 14);
+		lblTiempoKmR.setBounds(223, 95, 118, 14);
 		contentPane.add(lblTiempoKmR);
 	}
 	
